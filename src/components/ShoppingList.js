5d50834122ad125f266840b371aa7d1a15626c96
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-29 09:34:52 -0700

    initial display items

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,12 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((response) => response.json())
+      .then((item) => setItems(item));
+  }, []);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
